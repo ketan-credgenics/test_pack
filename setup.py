a976f510d907d2f6a41c356ac350f887517c89e0
ketan-credgenics <106948045+ketan-credgenics@users.noreply.github.com>
2022-06-06 05:18:35 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,14 @@
-import setuptools
-
-setuptools.setup(
-    name="package_ketan_test",
-    version="1.0",
-    packages=setuptools.find_packages(),
+from setuptools import setup, find_packages
+setup(
+    name='cg_database',
+    packages=find_packages(),
+    description='DB Library for Credgenics',
+    version='0.2.2',
+    url='',
+    author='Mehul Mittal',
+    author_email='mehul.mittal@credgenics.com',
+    install_requires = [
+        "Quart==0.17.0",
+        "asyncpg==0.21.0"
+    ]
 )
